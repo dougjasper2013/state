765cf07a21871877cc4ec57a9364d8ce56a20a61
Douglas Jasper <doug.jasper@trios.com>
2026-05-04 08:26:14 -0400

    Start Setup Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { useState, useCallback } from 'react';
+import { Header } from '@/components/Header';
+import { Main } from '@/components/Main';
+import { signIn, signOut } from '@/data/auth';
 
 export default function Home() {
+  const [userName, setUserName] = useState<
+    string | undefined
+  >();
+  const [permissions, setPermissions] = useState<
+    string[] | undefined
+  >();
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = useCallback(async () => {
+    setLoading(true);
+    const user = await signIn();
+    setUserName(user.name);
+    setPermissions(user.permissions);
+    setLoading(false);
+  }, []);
+  const handleSignOut = useCallback(async () => {
+    setLoading(true);
+    await signOut();
+    setUserName(undefined);
+    setPermissions(undefined);
+    setLoading(false);
+  }, []);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
